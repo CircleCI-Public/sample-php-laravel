8adb05582bb8b9b2c144ac8cdd9954769cf43fd1
teddywafula <teddywafula@gmail.com>
2019-12-12 12:28:46 +0300

    updates to home blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
 	<div class="row">
 		<div class="col-md-10 col-md-offset-1">
 			<div class="panel panel-default">
-				<div class="panel-heading">Home</div>
+				<div class="panel-heading">Homepage</div>
 
 				<div class="panel-body">
 					You are logged in!
